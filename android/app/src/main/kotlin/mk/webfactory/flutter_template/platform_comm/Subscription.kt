2d72b45c7b03f470729e771a0c3cff0f3252f26a
dicum3467-afk <dicum3467@gmail.com>
2026-02-16 19:52:42 +0200

    Remove comments and clean up Subscription class

--- android/app/src/main/kotlin/mk/webfactory/flutter_template/platform_comm/Subscription.kt
+++ android/app/src/main/kotlin/mk/webfactory/flutter_template/platform_comm/Subscription.kt
@@ -1,17 +1,1 @@
-package mk.webfactory.flutter_template.platform_comm;
-
-/**
- * Service subscription. Call [cancel] to unsubscribe.
- */
-class Subscription(private var cancel: (() -> Unit)?) {
-
-    /**
-     * Cancels the subscription.
-     *
-     * Can be called once. Subsequent calls have no effect.
-     */
-    fun cancel() {
-        cancel?.invoke()
-        cancel = null
-    }
-}
+package mk.webfactory.flutter_template.platform_comm

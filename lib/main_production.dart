b9801e8df75ee6e6a11f07876f37701cc31eb80e
dicum3467-afk <dicum3467@gmail.com>
2026-02-17 10:00:55 +0200

    Update main_production.dart

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -3,10 +3,5 @@ import 'app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
-  const k = String.fromEnvironment('APIFOOTBALL_KEY');
-  // ignore: avoid_print
-  print('APIFOOTBALL_KEY prefix: ${k.isEmpty ? "EMPTY" : k.substring(0, 4)}');
-
   runApp(const SurePredictApp());
 }

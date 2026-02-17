a2d44f909654e4bcc117baeefdb8efe9acce9162
dicum3467-afk <dicum3467@gmail.com>
2026-02-17 14:41:10 +0200

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,48 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'l10n/l10n.dart';
 import 'screens/home_screen.dart';
 
-class SurePredictApp extends StatefulWidget {
+class SurePredictApp extends StatelessWidget {
   const SurePredictApp({super.key});
-
-  @override
-  State<SurePredictApp> createState() => _SurePredictAppState();
-}
-
-class _SurePredictAppState extends State<SurePredictApp> {
-  Locale? _locale;
-
-  void _setLocale(Locale? locale) {
-    setState(() {
-      _locale = locale;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sure Predict',
       debugShowCheckedModeBanner: false,
-      locale: _locale,
-      supportedLocales: const [
-        Locale('ro'),
-        Locale('en'),
-      ],
+      title: 'Sure Predict',
+      theme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+      ),
       localizationsDelegates: const [
         AppL10n.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
       ],
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        useMaterial3: true,
-        colorSchemeSeed: const Color(0xFF00FF9C),
-      ),
-      home: HomeScreen(
-        onChangeLanguage: _setLocale,
-      ),
+      supportedLocales: AppL10n.supportedLocales,
+      home: const HomeScreen(),
     );
   }
 }

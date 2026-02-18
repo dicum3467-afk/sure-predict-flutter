a2c62fa3d64b7c9c4e5a4e20e5f96b2e318e4465
dicum3467-afk <dicum3467@gmail.com>
2026-02-18 13:31:16 +0200

    Update db.py

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,1 +1,9 @@
+import os
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
+def get_conn():
+    db_url = os.getenv("DATABASE_URL")
+    if not db_url:
+        raise RuntimeError("DATABASE_URL is not set")
+    return psycopg2.connect(db_url, cursor_factory=RealDictCursor)

d051dcda290167f7ad1a735fdc264f2c250e6182
dicum3467-afk <dicum3467@gmail.com>
2026-02-22 09:39:40 +0200

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'api/api_client.dart';
+import 'services/sure_predict_service.dart';
+import 'state/leagues_store.dart';
+import 'ui/leagues_screen.dart';
+
+void main() {
+  runApp(const SurePredictApp());
+}
+
+class SurePredictApp extends StatelessWidget {
+  const SurePredictApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final api = ApiClient(baseUrl: 'https://sure-predict-backend.onrender.com');
+    final service = SurePredictService(api);
+    final leaguesStore = LeaguesStore(service);
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Sure Predict',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: LeaguesScreen(store: leaguesStore),
+    );
+  }
+}

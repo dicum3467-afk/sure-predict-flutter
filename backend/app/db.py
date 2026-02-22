b04a2b674587e3d2fcdfa2f0792dcb0216337881
dicum3467-afk <dicum3467@gmail.com>
2026-02-22 09:17:26 +0200

    Update db.py

--- backend/app/db.py
+++ backend/app/db.py
@@ -6,4 +6,9 @@ def get_conn():
     db_url = os.getenv("DATABASE_URL")
     if not db_url:
         raise RuntimeError("DATABASE_URL is not set")
-    return psycopg2.connect(db_url, cursor_factory=RealDictCursor)
+
+    return psycopg2.connect(
+        db_url,
+        cursor_factory=RealDictCursor,
+        sslmode="require"
+    )

9d9c8f7076bf62ee95045579174f82c8d4c29bd7
dicum3467-afk <dicum3467@gmail.com>
2026-02-22 23:10:49 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 import 'api/api_client.dart';
 import 'services/sure_predict_service.dart';
-import 'state/leagues_store.dart';
-import 'state/fixtures_store.dart';
 import 'state/favorites_store.dart';
+import 'state/leagues_store.dart';
 import 'ui/home_shell.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await MobileAds.instance.initialize();
+void main() {
   runApp(const SurePredictApp());
 }
 
@@ -23,20 +19,18 @@ class SurePredictApp extends StatelessWidget {
     final service = SurePredictService(api);
 
     final leaguesStore = LeaguesStore(service);
-    final fixturesStore = FixturesStore(service);
     final favoritesStore = FavoritesStore();
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Sure Predict',
       theme: ThemeData(
-        useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-        scaffoldBackgroundColor: const Color(0xFFF6F7F9),
+        useMaterial3: true,
       ),
       home: HomeShell(
+        service: service,
         leaguesStore: leaguesStore,
-        fixturesStore: fixturesStore,
         favoritesStore: favoritesStore,
       ),
     );

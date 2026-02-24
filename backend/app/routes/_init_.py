e088f70a7ef0ed796189887964abfd4b48b1fb03
dicum3467-afk <dicum3467@gmail.com>
2026-02-24 04:39:53 +0200

    Update _init_.py

--- backend/app/routes/_init_.py
+++ backend/app/routes/_init_.py
@@ -1,5 +1,9 @@
+from .leagues import router as leagues_router
+from .fixtures_by_league import router as fixtures_by_league_router
+from .fixtures_sync import router as fixtures_sync_router
 
-from . import leagues
-from . import fixtures
-from . import predictions
-from . import fixtures_by_league
+__all__ = [
+    "leagues_router",
+    "fixtures_by_league_router",
+    "fixtures_sync_router",
+]

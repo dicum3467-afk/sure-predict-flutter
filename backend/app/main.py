11e36625f231b773c58e1176279619a304d62151
dicum3467-afk <dicum3467@gmail.com>
2026-02-26 04:57:15 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-# import corect pe fișiere
 from app.routes.leagues import router as leagues_router
-from app.routes.fixtures_by_league import router as fixtures_by_league_router
+from app.routes.fixtures import router as fixtures_router
+from app.routes.prediction import router as prediction_router
 from app.routes.fixtures_sync import router as fixtures_sync_router
 
 app = FastAPI(title="Sure Predict Backend")
@@ -19,9 +19,9 @@ app.add_middleware(
 
 # include routers
 app.include_router(leagues_router)
-app.include_router(fixtures_by_league_router)
+app.include_router(fixtures_router)
+app.include_router(prediction_router)
 app.include_router(fixtures_sync_router)
-
 
 @app.get("/health")
 def health():

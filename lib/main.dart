40a86892bab316647cd87f2127a4ef6489d31bc5
dicum3467-afk <dicum3467@gmail.com>
2026-03-06 16:24:31 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,36 @@
 import 'package:flutter/material.dart';
-
-import 'services/api_client.dart';
-import 'services/sure_predict_service.dart';
-
-import 'state/leagues_store.dart';
-import 'state/favorites_store.dart';
-
-import 'ui/home_shell.dart';
+import 'screens/app_shell.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  const baseUrl = 'https://sure-predict-backend.onrender.com';
-
-  final api = ApiClient(baseUrl: baseUrl);
-  final service = SurePredictService(api);
-
-  final leaguesStore = LeaguesStore(service);
-  final favoritesStore = FavoritesStore();
-
-  runApp(SurePredictApp(
-    service: service,
-    leaguesStore: leaguesStore,
-    favoritesStore: favoritesStore,
-  ));
+  runApp(const SurePredictApp());
 }
 
 class SurePredictApp extends StatelessWidget {
-  final SurePredictService service;
-  final LeaguesStore leaguesStore;
-  final FavoritesStore favoritesStore;
-
-  const SurePredictApp({
-    super.key,
-    required this.service,
-    required this.leaguesStore,
-    required this.favoritesStore,
-  });
+  const SurePredictApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Sure Predict',
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
       theme: ThemeData(
         useMaterial3: true,
         colorSchemeSeed: Colors.blue,
       ),
-      home: HomeShell(
-        service: service,
-        leaguesStore: leaguesStore,
-        favoritesStore: favoritesStore,
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        colorSchemeSeed: Colors.blue,
+        scaffoldBackgroundColor: const Color(0xFF0B1220),
+        cardTheme: CardTheme(
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
       ),
+      home: const AppShell(),
     );
   }
 }

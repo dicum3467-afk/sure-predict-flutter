a457a37630fcb51fe16fddecfc5670667eb36ec8
dicum3467-afk <dicum3467@gmail.com>
2026-03-06 21:03:00 +0200

    Update __init__.py

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -4,6 +4,7 @@ from .fixtures import router as fixtures_router
 from .fixtures_by_league import router as fixtures_by_league_router
 from .leagues import router as leagues_router
 from .teams import router as teams_router
+from .predictions import router as predictions_router
 
 api_router = APIRouter()
 
@@ -11,3 +12,4 @@ api_router.include_router(fixtures_router)
 api_router.include_router(fixtures_by_league_router)
 api_router.include_router(leagues_router)
 api_router.include_router(teams_router)
+api_router.include_router(predictions_router)

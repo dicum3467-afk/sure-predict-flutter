81015768c5d42bd976b8ae8c713761fef68d548b
dicum3467-afk <dicum3467@gmail.com>
2026-03-06 16:27:13 +0200

    Create app_shell.dart

---
+++ lib/screens/app_shell.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'fixtures_screen.dart';
+import 'home_screen.dart';
+import 'profile_screen.dart';
+
+class AppShell extends StatefulWidget {
+  const AppShell({super.key});
+
+  @override
+  State<AppShell> createState() => _AppShellState();
+}
+
+class _AppShellState extends State<AppShell> {
+  int currentIndex = 0;
+
+  late final List<Widget> screens = [
+    const HomeScreen(),
+    const FixturesScreen(),
+    const ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screens[currentIndex],
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: currentIndex,
+        onDestinationSelected: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.home_outlined),
+            selectedIcon: Icon(Icons.home_rounded),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.sports_soccer_outlined),
+            selectedIcon: Icon(Icons.sports_soccer_rounded),
+            label: 'Fixtures',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person_outline_rounded),
+            selectedIcon: Icon(Icons.person_rounded),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}

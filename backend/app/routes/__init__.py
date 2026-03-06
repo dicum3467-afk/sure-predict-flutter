d65609d958c4fc0b5859156564c2596fe35273b6
dicum3467-afk <dicum3467@gmail.com>
2026-03-06 18:48:37 +0200

    Update __init__.py

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,6 +1,53 @@
 from fastapi import APIRouter
+
 from .fixtures import router as fixtures_router
+from .fixtures_by_league import router as fixtures_by_league_router
+
+from .leagues import router as leagues_router
+from .teams import router as teams_router
+
+from .predictions import router as predictions_router
+from .odds import router as odds_router
+from .value import router as value_router
+
+from .admin import router as admin_router
+from .admin_hub import router as admin_hub_router
+
+from .leagues_sync import router as leagues_sync_router
+from .teams_sync import router as teams_sync_router
+from .fixtures_sync import router as fixtures_sync_router
+
+from .evaluation import router as evaluation_router
+from .job import router as job_router
+
 
 api_router = APIRouter()
 
+
+# PUBLIC
 api_router.include_router(fixtures_router)
+api_router.include_router(fixtures_by_league_router)
+api_router.include_router(leagues_router)
+api_router.include_router(teams_router)
+api_router.include_router(predictions_router)
+api_router.include_router(odds_router)
+api_router.include_router(value_router)
+
+
+# ADMIN
+api_router.include_router(admin_router)
+api_router.include_router(admin_hub_router)
+
+
+# SYNC JOBS
+api_router.include_router(leagues_sync_router)
+api_router.include_router(teams_sync_router)
+api_router.include_router(fixtures_sync_router)
+
+
+# EVALUATION
+api_router.include_router(evaluation_router)
+
+
+# JOB
+api_router.include_router(job_router)

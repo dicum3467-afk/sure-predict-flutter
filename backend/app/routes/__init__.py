0cf19e7d68838dcd452b7c4f9262729616242684
dicum3467-afk <dicum3467@gmail.com>
2026-03-06 22:20:17 +0200

    Update __init__.py

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from .fixtures import router as fixtures_router
 from .predictions import router as predictions_router
+from .team_stats import router as team_stats_router
 
 api_router = APIRouter()
 
 api_router.include_router(fixtures_router)
 api_router.include_router(predictions_router)
+api_router.include_router(team_stats_router)

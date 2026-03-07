e77626d94f58ac247c401f898e81f4d13fd1e549
dicum3467-afk <dicum3467@gmail.com>
2026-03-07 19:22:29 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,28 +13,17 @@ class SurePredictApp extends StatelessWidget {
     return MaterialApp(
       title: 'Sure Predict',
       debugShowCheckedModeBanner: false,
-
       themeMode: ThemeMode.dark,
-
       theme: ThemeData(
         useMaterial3: true,
         colorSchemeSeed: Colors.blue,
       ),
-
       darkTheme: ThemeData(
         useMaterial3: true,
         brightness: Brightness.dark,
         colorSchemeSeed: Colors.blue,
         scaffoldBackgroundColor: const Color(0xFF0B1220),
-
-        cardTheme: CardTheme(
-          elevation: 0,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
-          ),
-        ),
       ),
-
       home: const FixturesScreen(),
     );
   }

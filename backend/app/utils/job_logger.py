5285eecb5ab5946412e7b568c559930176de65c0
dicum3467-afk <dicum3467@gmail.com>
2026-03-07 20:35:07 +0200

    Create job_logger.py

---
+++ backend/app/utils/job_logger.py
@@ -0,0 +1,12 @@
+from app.db import get_conn
+
+
+def log_job(job_name: str, status: str, details: str = ""):
+    sql = """
+        insert into job_runs (job_name, status, details)
+        values (%s, %s, %s)
+    """
+    with get_conn() as conn:
+        with conn.cursor() as cur:
+            cur.execute(sql, (job_name, status, details))
+        conn.commit()

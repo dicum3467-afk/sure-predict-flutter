16e9ad6b9fda09f813478ca43a8e52204209d926
dicum3467-afk <dicum3467@gmail.com>
2026-03-12 05:14:39 +0200

    Update run_daily_pipeline.py

--- backend/app/jobs/run_daily_pipeline.py
+++ backend/app/jobs/run_daily_pipeline.py
@@ -1,5 +1,3 @@
-from app.jobs.sync_leagues import run as run_sync_leagues
-from app.jobs.sync_teams import run as run_sync_teams
 from app.jobs.sync_fixtures import run as run_sync_fixtures
 from app.jobs.sync_results import run as run_sync_results
 from app.jobs.rebuild_team_stats import run as run_rebuild_team_stats
@@ -7,8 +5,6 @@ from app.jobs.rebuild_team_elo import run as run_rebuild_team_elo
 
 
 def run():
-    run_sync_leagues()
-    run_sync_teams()
     run_sync_fixtures()
     run_sync_results()
     run_rebuild_team_stats()
